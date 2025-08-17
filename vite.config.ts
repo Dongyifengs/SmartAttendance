cceb21cbbc2ce65fc6e15129ee846dca312cf05c
MoYi <1545929126@qq.com>
2025-08-18 00:21:06 +0800

    [配置文件修改🔧]|添加Server配置，添加作者信息与证书名称。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// 配置文件文档: https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  // Plugins Vite 插件
+  plugins: [
+      vue()
+  ],
+  // 服务器选项
+  server: {
+      port: 3000,
+      host: '0.0.0.0',
+      open: true
+  }
 })
