bfaba9e984ff01d311db2b390f542a7c92e65450
MoYi <1545929126@qq.com>
2025-08-18 23:33:43 +0800

    [新功能✨]|添加UI组件和自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // 配置文件文档: https://vite.dev/config/
 export default defineConfig({
   // Plugins Vite 插件
   plugins: [
-      vue()
+      vue(),
+      AutoImport({
+          resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+          resolvers: [ElementPlusResolver()],
+      }),
   ],
   // 服务器选项
   server: {
