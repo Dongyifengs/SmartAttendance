9787f07d6d64be02eaace3ff3f5c0c50786ba5aa
MoYi <1545929126@qq.com>
2025-09-02 15:37:52 +0800

    修改项目部分代码

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue()
+  ],
+  server: {
+      port: 3000,
+      host: '0.0.0.0',
+      open: true
+  }
 })
