8759c7ec992fef8de2c6dccd11cedc4b135f382a
MoYi <1545929126@qq.com>
2025-10-11 16:02:05 +0800

    添加路由跳转

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,9 @@ import {createRouter, createWebHashHistory} from "vue-router";
 
 import Login from "../view/Login/Login.vue";
 import Home from "../view/Home/Home.vue"
-import ClassListDev from "../view/ClassListDev/ClassListDev.vue"
+import ClassListDev from "../view/ZHKQ-DEV/ClassListDev.vue"
+import AttendanceDev from "../view/ZHKQ-DEV/AttendanceDev.vue";
+import LeaveDev from "../view/ZHKQ-DEV/LeaveDev.vue"
 
 // 路由配置
 export const routes = [
@@ -28,6 +30,22 @@ export const routes = [
         meta: {
             requiresAuth: true,
             title: "开发者课程详细列表"
+        }
+    },
+    {
+        path: '/AttendanceDev',
+        component: AttendanceDev,
+        meta: {
+            requiresAuth: true,
+            title: "开发者考勤列表"
+        }
+    },
+    {
+        path: '/LeaveDev',
+        component: LeaveDev,
+        meta: {
+            requiresAuth: true,
+            title: "开发者请假列表"
         }
     }
 ]
