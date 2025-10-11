feb3fef2b6e98fe6578e8722bbee4052cd8e3247
MoYi <1545929126@qq.com>
2025-10-11 16:00:44 +0800

    添加开发者考勤页面

---
+++ src/view/ZHKQ-DEV/AttendanceDev.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-text>AttendanceDev - 考勤查询页面</el-text>
+  <el-button style="margin-left: 10px" @click="$router.back()">返回上一页</el-button>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
