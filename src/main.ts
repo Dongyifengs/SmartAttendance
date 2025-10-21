ad211dc48e11cb22dab5353c615751c28bec8743
MoYi <1545929126@qq.com>
2025-10-21 13:45:41 +0800

    动态统计分析

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'element-plus/theme-chalk/el-message.css'
 
 const app = createApp(App)
+
+// 动态插入统计脚本
+const script: HTMLScriptElement = document.createElement('script')
+script.defer = true
+script.src = import.meta.env.VITE_STAT_SCRIPT
+script.dataset.websiteId = import.meta.env.VITE_STAT_WEBSITE_ID
+document.head.appendChild(script)
 
 app.use(router)
 app.mount('#app')
