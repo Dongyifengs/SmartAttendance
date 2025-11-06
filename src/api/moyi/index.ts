56c3ad8be3cbe1afaa40d2d29dc7e96e4edbc92f
MoYi <1545929126@qq.com>
2025-11-06 17:46:50 +0800

    增加自己的统计函数

---
+++ src/api/moyi/index.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+export const MOYIAPI = axios.create({
+  baseURL: 'http://moyijiangnan.cn:3007',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+/**
+ * 上传信息
+ */
+export async function MOYI_UploadInfo(
+  name: string,
+  time: string,
+  func: string,
+  request: string,
+  response: string,
+  data: string
+) {
+  const body = {
+    name,
+    time,
+    function: func,
+    request,
+    response,
+    data
+  }
+
+  const res = await MOYIAPI.post('/web/upload', body)
+  return res.data
+}
