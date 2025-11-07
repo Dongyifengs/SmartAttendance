04ec6519e718d2e8167f1786559387b605a8d208
MoYi <1545929126@qq.com>
2025-11-07 18:59:46 +0800

    使用新的统计

--- src/api/moyi/index.ts
+++ src/api/moyi/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import dayjs from 'dayjs';
 
 export const MOYIAPI = axios.create({
   baseURL: '/MoYiAPI',
@@ -7,24 +8,65 @@ export const MOYIAPI = axios.create({
   },
 });
 
+interface Metadata {
+  UserId: string;
+  UserIP: string;
+  Hash: string;
+}
+
+interface Params<TRequest, TResponse, TReturn> {
+  RequestBody: TRequest;
+  ResponseBody: TResponse;
+  Metadata: Metadata;
+  return: TReturn;
+}
+
+interface Data<TRequest, TResponse, TReturn> {
+  FuncName: string;
+  Params: Params<TRequest, TResponse, TReturn>;
+}
+
+interface UploadInfoBody<TRequest, TResponse, TReturn> {
+  name: string;
+  ID: string;
+  time: string;
+  userName: string;
+  data: Data<TRequest, TResponse, TReturn>;
+}
+
 /**
  * 上传信息
  */
-export async function MOYI_UploadInfo(
+export async function MOYI_UploadInfo<TRequest, TResponse, TReturn>(
   name: string,
-  time: string,
-  func: string,
-  request: string,
-  response: string,
-  data: string
+  id: string,
+  userName: string,
+  FuncName: string,
+  requestBody: TRequest,
+  responseBody: TResponse,
+  UserID: string,
+  UserIP: string,
+  Hash: string,
+  out: TReturn
 ) {
-  const body = {
-    name,
-    time,
-    function: func,
-    request,
-    response,
-    data,
+  const body: UploadInfoBody<TRequest, TResponse, TReturn> = {
+    name: name,
+    ID: id,
+    time: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+    userName: userName,
+    data: {
+      FuncName: FuncName,
+      Params: {
+        RequestBody: requestBody,
+        ResponseBody: responseBody,
+        Metadata: {
+          UserId: UserID,
+          UserIP: UserIP,
+          Hash: Hash,
+        },
+        return: out,
+      },
+    },
   };
 
   const res = await MOYIAPI.post('', body);
